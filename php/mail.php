95b9ad08bacc34f55ec8cd8275eeb82ecdc66e58
Gama\Gamaliel <gamaliellobato@gmail.com>
2017-01-18 16:13:39 -0600

    Se agrega libreria smtp para envio de correo

--- php/mail.php
+++ php/mail.php
@@ -1,27 +1,87 @@
 <?php
-// Guardar los datos recibidos en variables:
+
+//Librerías para el envío de mail
+include_once('class.phpmailer.php');
+include_once('class.smtp.php');
+
+//Recibir todos los parámetros del formulario
+$para = "gamaliellobato@gmail.com";
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
-$telefono = $_POST['telefono'];
-$mensaje = $_POST['mensaje'];
-// Definir el correo de destino:
-$dest = "gamaliellobato@gmail.com"; 
- 
-// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-$headers = "From: $nombre <$email>\r\n";  
-$headers .= "X-Mailer: PHP5\n";
-$headers .= 'MIME-Version: 1.0' . "\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Aqui definimos el asunto y armamos el cuerpo del mensaje
-$asunto = "Contacto";
-$cuerpo = "Nombre: ".$nombre."<br>";
-$cuerpo .= "Email: ".$email."<br>";
-$cuerpo .= "Telefono: ".$telefono."<br>";
-$cuerpo .= "Mensaje: ".$mensaje;
- 
-// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
-if($nombre != '' && $email != '' && $telefono != '' && $mensaje != ''){
-    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
+$tel = $_POST['tel'];
+$comen = $_POST['mensaje'];
+$mensaje = '<html><body>
+            <center>
+              <h1>Groove</h1>
+            </center>
+            <br>
+            <br>
+            <center>
+              <img src="" alt="icono">
+            </center>
+
+
+            <br>
+            <br>
+              
+            <center>
+              <span><p>Nombre:'.$nombre.'</p></span>
+
+              <br>
+
+              <span><p>Email:&nbsp;'.$email.'</p></span>
+
+              <br>
+
+              <span><p>Telefono:'.$tel.'</p></span>
+
+              <br>
+
+              <span><p>Mensaje:'.$comen.'</p></span>
+            </center>
+
+            </body></html>
+    ';
+
+
+
+$asunto = "Nuevo mensaje de red convergencia";
+
+//Este bloque es importante
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "ssl";
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465;
+
+//Nuestra cuenta
+$mail->Username ='gamaliellobato@gmail.com';
+$mail->Password = 'xuqrzmzuxdcmzcfn';
+
+//Agregar destinatario
+$mail->AddAddress($para);
+$mail->Subject = $asunto;
+//$mail->AltBody = $nombre;
+//$mail->AltBody = $email;
+//$mail->Body = $tel;
+$mail->Body = $mensaje;
+//Para adjuntar archivo
+$mail->AddAttachment($archivo['tmp_name'], $archivo['name']);
+$mail->MsgHTML($mensaje);
+
+//Avisar si fue enviado o no y dirigir al index
+if($mail->Send())
+{
+	echo'<script type="text/javascript">
+			alert("Enviado Correctamente");
+		 </script>';
+}
+else{
+	echo'<script type="text/javascript">
+			alert("NO ENVIADO, intentar de nuevo");
+		 </script>';
 }
 ?>
+
+
